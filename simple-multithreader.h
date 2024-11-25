1e2f8442c436e0d38c0e689805f115463d4ec9f9
Pratyush Gangwar <pratyush23395@iiitd.ac.in>
2024-11-25 10:02:36 +0530

    2D multithreading

--- simple-multithreader.h
+++ simple-multithreader.h
@@ -55,11 +55,35 @@ struct linear_args {
   std::function<void(int)> lambda;
 };
 
+struct matrix_args {
+  int linear_low;
+  int linear_high;
+
+  int cols;
+
+  std::function<void(int, int)> lambda;
+};
+
 void* linear_thread_func(void* args) {
   struct linear_args* lin_args = (struct linear_args*) args;
 
   for(int i = lin_args->low; i < lin_args->high; i++) {
     (lin_args->lambda)(i);
+  }
+
+  return NULL;
+}
+
+void* matrix_thread_func(void* args) {
+  struct matrix_args* matrix_args = (struct matrix_args*) args;
+
+  for(int i = matrix_args->linear_low; i < matrix_args->linear_high; i++) {
+    int row = i / matrix_args->cols;
+    int col = i % matrix_args->cols;
+
+    // printf("%d %d\n", row, col);
+
+    (matrix_args->lambda)(row, col);
   }
 
   return NULL;
@@ -72,13 +96,40 @@ void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numT
   pthread_t* tid_array = (pthread_t*) malloc(sizeof(pthread_t) * numThreads);
   struct linear_args* linear_args_array = (struct linear_args*)  malloc( sizeof(struct linear_args) * numThreads );
 
-  int total_elements = high - low + 1;
+  // for(int i = low; i < high; i++)
+  // (high - 1) - low + 1
+  int total_elements = high - low;
   int chunk_size = (total_elements)/numThreads;
+
+
+  // ~~~ Proof that high never goes out of bounds ~~~
+  // note, chunk_size = floor( total_elements/numThreads )
+  // floor( total_elements/numThreads ) <= total_elements/numThreads
+  // so, chunk_size <= total_elements/numThreads
+  // so, chunk_size * numThreads <= total_elements
+
+  // high is (i + 1) * chunk_size
+  // max value of i is numThreads - 1
+  // so, max value of high is numThreads * chunk_size
+  // so, high will never go out of bounds
+
+  // ~~~ Proof that if total_elements % numThreads != 0, then some elements may be missed ~~~
+  // when total_elements % numThreads = 0, total_elements/numThreads is an integer and floor(total_elements/numThreads) = total_elements/numThreads
+  // so, chunk_size = total_elements/numThreads. so, chunk_size * numThreads = total_elements
+  // so, the high value for the last thread is ((num_threads - 1) + 1) * chunk_size = total_elements. so, no elements are missed
+
+  // but when total_elements % numThreads != 0, total_elements/numThreads is not an integer and floor(total_elements/numThreads) < total_elements/numThreads
+  // so, chunk_size < total_elements/numThreads
+  // so, chunk_size * numThreads < total_elements
+  // so the high value for the last thread is ( (num_threads - 1) + 1 ) * chunk_size < total_elements. so, elements are definitely missed.
+  // therefore, we need the last thread to cover some extra elements
 
   for(int i = 0; i < numThreads; i++) {
     linear_args_array[i].low = i * chunk_size;
-    linear_args_array[i].high = min( (i + 1) * chunk_size, total_elements );
+    linear_args_array[i].high = ( i == numThreads - 1 ? total_elements : (i + 1) * chunk_size);
     linear_args_array[i].lambda = lambda;
+
+    // printf("thread %d: %d %d\n", i, linear_args_array[i].low, linear_args_array[i].high);
 
     pthread_create( &tid_array[i], NULL, linear_thread_func, (void*) &linear_args_array[i] );
   }
@@ -90,14 +141,50 @@ void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numT
   free(tid_array);
   free(linear_args_array);
 }
-
-
  
 // This version of parallel_for is for parallelizing two-dimensional for-loops, i.e., an outter for-i loop and  
 // an inner for-j loop. Loop properties, i.e. low, high are mentioned below for both outter  
 // and inner for-loops. The suffixes “1” and “2” represents outter and inner loop properties respectively.  
-void parallel_for(int low1, int high1,  int low2, int high2, 
-         std::function<void(int, int)>  &&lambda, int numThreads) {
+void parallel_for(int low1, int high1,  int low2, int high2, std::function<void(int, int)>  &&lambda, int numThreads) {
 
-         }
+  // note (row, col) in 2D array corresponds to row * num_cols + col in 1D array
+  // linear_idx in 1D has row = linear_idx/num_cols and col = linear_idx % num_cols 
+
+  pthread_t* tid_array = (pthread_t*) malloc(sizeof(pthread_t) * numThreads);
+  struct matrix_args* matrix_args_array = (struct matrix_args*)  malloc( sizeof(struct matrix_args) * numThreads );
+
+  // for(int j = low2; j < high2; j++)
+  // (high2 - 1) - low2 + 1
+  int num_cols = high2 - low2;
+
+  // start at (low1, low2) and end at (high1 - 1, high2 - 1) (included)
+  int linear_start_idx = low1 * num_cols + low2;
+  int linear_end_idx = (high1 - 1) * num_cols + (high2 - 1);
+
+  // (end_idx) - start_idx + 1
+  int total_elements = linear_end_idx - linear_start_idx + 1;
+  int chunk_size = (total_elements)/numThreads;
+
+  // printf("low1: %d high1: %d low2: %d high2: %d\n", low1, high1, low2, high2);
+  // printf("cols: %d start: %d end: %d total: %d sz: %d\n", num_cols, linear_start_idx, linear_end_idx, total_elements, chunk_size);
+
+  for(int i = 0; i < numThreads; i++) {
+
+    matrix_args_array[i].linear_low = i * chunk_size;
+    matrix_args_array[i].linear_high = ( i == numThreads - 1 ? total_elements : (i + 1) * chunk_size);
+    matrix_args_array[i].cols = num_cols;
+    matrix_args_array[i].lambda = lambda;
+
+    // printf("thread %d: %d %d\n", i, matrix_args_array[i].linear_low ,  matrix_args_array[i].linear_high );
+
+    pthread_create( &tid_array[i], NULL, matrix_thread_func, (void*) &matrix_args_array[i] );
+  }
+
+  for(int i = 0; i < numThreads; i++) {
+    pthread_join( tid_array[i], NULL );
+  }
+
+  free(tid_array);
+  free(matrix_args_array);
+}
 
